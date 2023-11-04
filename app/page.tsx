a27f4aa675d79859d54a9ab7a7a946b19feef2e7
4abaev <progchab@gmail.com>
2023-11-04 15:48:04 +0300

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import styles from './page.module.css'
 import GetAssetsPage from "@/components/getAssetsPage";
 
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Get assets from website',
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
